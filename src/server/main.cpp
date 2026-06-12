e0d551853b410b68dab015828b2fc3ce4cfb68d1
nightmareblackengel <nightmareblackengel@gmail.com>
2026-06-12 14:28:40 +0300

    MyQueue part2

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,102 +3,149 @@
 #include "Server01.h"
 
 #include <string>
+#include <exception>
 
 using std::string;
 
-template <typename QueueItem>
-struct QueueItemStruct
+template <typename QItem1>
+class MyQueue;
+
+template <typename QItem1>
+class QueueItem
 {
-    QueueItem item;
-    QueueItemStruct* next;
-    QueueItemStruct* prev;
+    QItem1 item;
+    QueueItem* next;
+    QueueItem* prev;
+
+    friend class MyQueue<QItem1>;
+public:
+    QueueItem(QItem1 item):item(item)
+    {
+        
+    }
 };
 
-template <typename QueueItem>
+template <typename QItem1>
 class MyQueue
 {
-    static QueueItemStruct<QueueItem>* top     = nullptr;
-    static QueueItemStruct<QueueItem>* bottom  = nullptr;
+    QueueItem<QItem1>* start   = nullptr;
+    QueueItem<QItem1>* end     = nullptr;
+    int count = 0;
+    // TODO: REMOVE?
+//    QueueItem<QItem1>* current = nullptr;
 public:
 
-    MyQueue()= delete;
-    MyQueue(QueueItem &_item)
+    MyQueue()
     {
-        this->item = _item;
+        
     }
     ~MyQueue()
     {
-//        delete this->item;
+        this->removeAll();
+    }
+
+    void removeAll()
+    {
+        if (this->end == nullptr) {
+            return;
+        }
+
+        QueueItem<QItem1>* ptr = this->end;
+
+        while (ptr->next != nullptr) {
+            ptr = ptr->next;
+            delete ptr->prev;
+            ptr->prev = nullptr;
+            //this->count--;
+        }
+        delete ptr;
+
+        this->end   = nullptr;
+        this->start = nullptr;
+        this->count = 0;
+    }
+
+    int getCount()
+    {
+        return this->count;
     }
 
     // to next
-    void push_next(QueueItem _item)
+    void push_start(QItem1 _item)
     {
-        if (MyQueue::top == nullptr) {
-            MyQueue::top = new MyQueue(_item);
-            MyQueue::bottom = MyQueue::top;
-            this->next = nullptr;
-            this->prev = nullptr;
+        if (this->start == nullptr) {
+            QueueItem<QItem1>* newItem = new QueueItem<QItem1>(_item);
+
+            this->start = newItem;
+            this->end   = newItem;
+
+            newItem->next = nullptr;
+            newItem->prev = nullptr;
         } else {
-            MyQueue::top->next = new MyQueue(_item);
-            MyQueue::top->next->prev = this;
-            MyQueue::top->next->next = nullptr;
-            // step forward
-            MyQueue::top = MyQueue::top->next;
+            QueueItem<QItem1>* newItem = new QueueItem<QItem1>(_item);
+
+            this->start->next = newItem;
+            newItem->prev = this->start;
+            newItem->next = nullptr;
+            // do step
+            this->start = newItem;
         }
+        this->count++;
     }
 
-    QueueItem pop_next()
+    QItem1 pop_start()
     {
-        QueueItem result = nullptr;
-        if (MyQueue::bottom == nullptr) {
+        if (this->start == nullptr) {
+            throw "Warning. The queue is empty.";
+        }
 
-        } else if (MyQueue::bottom == MyQueue::top) {
-            result = MyQueue::bottom->item;
-            delete MyQueue::bottom;
-            MyQueue::bottom = nullptr;
-            MyQueue::top = nullptr;
-        } else if (MyQueue::bottom != MyQueue::top) {
-            MyQueue *ptr = MyQueue::top;
-            MyQueue::top = MyQueue::top->prev;
-            ptr->prev = nullptr;
-//            ptr->next = nullptr;
-            result = ptr->item;
+        QItem1 result = this->start->item;
+        if (this->start == this->end) {
+            delete this->start;
+
+            this->start = nullptr;
+            this->end   = nullptr;
+            this->count = 0;
+        } else {
+            QueueItem<QItem1> *ptr = this->start;
+            this->start = ptr->prev;
+            this->start->next = nullptr;
             delete ptr;
+            this->count--;
         }
 
         return result;
     }
 
-
+/*
     // prev
-    void push_prev(QueueItem _item)
+    void push_end(QItem1 _item)
     {
-        if (MyQueue::bottom == nullptr) {
-            MyQueue::bottom = new MyQueue(_item);
-            MyQueue::top = MyQueue::bottom;
-            MyQueue::bottom->prev = nullptr;
-            MyQueue::bottom->next = nullptr;
+        if (this->end == nullptr) {
+            this->end = new QueueItem(_item);
+            this->start = this->end;
+            this->end->prev = nullptr;
+            this->end->next = nullptr;
         } else {
-            MyQueue::bottom->prev = new MyQueue(_item);
-            MyQueue::bottom->prev->prev = nullptr;
-            MyQueue::bottom->prev->next = this;
-            MyQueue::bottom = MyQueue::bottom->prev;
+            this->end->prev = new QueueItem(_item);
+            this->end->prev->prev = nullptr;
+            this->end->prev->next = this;
+            this->end = this->end->prev;
         }
     }
-    QueueItem pop_prev()
+    QItem1 pop_end()
     {
-        QueueItem result = nullptr;
-        if (MyQueue::bottom == nullptr) {
+        QItem1 result = nullptr;
+        if (this->end == nullptr) {
 
-        } else if (MyQueue::bottom == MyQueue::top) {
-            result = MyQueue::bottom->item;
-            delete MyQueue::bottom;
-            MyQueue::bottom = nullptr;
-            MyQueue::top = nullptr;
-        } else if (MyQueue::bottom != MyQueue::top) {
-            MyQueue* ptr = MyQueue::bottom;
-            MyQueue::bottom = MyQueue::bottom->next;
+        } else if (this->end == this->start) {
+            result = this->end->item;
+            delete this->end;
+            this->end = nullptr;
+            this->start = nullptr;
+        } else if (this->end != this->start) {
+            QueueItem* ptr = this->end;
+            this->end = this->end->next;
             result = ptr->item;
             ptr->next = nullptr;
 //            ptr->prev = nullptr;
@@ -106,6 +153,7 @@ public:
         }
         return result;
     }
+    /**/
 
 //    void getItem()
 //    {
@@ -120,10 +168,31 @@ class ConsumerProducer
 };
 
 int main() {
-    MyQueue mq();
+    try {
+        MyQueue<int> mq{};
+        mq.push_start(77);
+        mq.push_start(78);
 
+        int val11 = mq.pop_start();
+        int val12 = mq.pop_start();
 
+        MyQueue<int> mq2{};
 
+        MyQueue<int> mq3{};
+        mq3.push_start(88);
+        //int val2 = mq2.pop_start();
+        //int val3 = mq3.pop_start();
+
+        cout << "q1=" << mq.getCount() << endl;
+        cout << "q2=" << mq2.getCount() << endl;
+        cout << "q3=" << mq3.getCount() << endl;
+    } catch (const char* msg) {
+        cout << "Exc=" << msg << endl;
+    }
+
+//    mq.pus
+
+    cout  << "NED" << endl;
 
 
 
