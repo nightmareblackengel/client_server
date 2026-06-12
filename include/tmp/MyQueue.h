69a09a960829930713d86e3b8309acce03e65c83
nightmareblackengel <nightmareblackengel@gmail.com>
2026-06-12 17:30:14 +0300

    Added GoogleTests

---
+++ include/tmp/MyQueue.h
@@ -0,0 +1,154 @@
+#ifndef NBE_CHAT_SERVER_MYQUEUE_H
+#define NBE_CHAT_SERVER_MYQUEUE_H
+
+template <typename QItem1>
+class MyQueue;
+
+template <typename QItem1>
+class QueueItem
+{
+    QItem1 item;
+    QueueItem* next;
+    QueueItem* prev;
+
+    friend class MyQueue<QItem1>;
+public:
+    QueueItem(QItem1 item):item(item)
+    {
+
+    }
+};
+
+template <typename QItem1>
+class MyQueue
+{
+    QueueItem<QItem1>* start   = nullptr;
+    QueueItem<QItem1>* end     = nullptr;
+    int count = 0;
+public:
+
+    MyQueue()
+    {
+
+    }
+    ~MyQueue()
+    {
+        this->removeAll();
+    }
+
+    void removeAll()
+    {
+        if (this->end == nullptr) {
+            return;
+        }
+
+        QueueItem<QItem1>* ptr = this->end;
+
+        while (ptr->next != nullptr) {
+            ptr = ptr->next;
+            delete ptr->prev;
+            ptr->prev = nullptr;
+            //this->count--;
+        }
+        delete ptr;
+
+        this->end   = nullptr;
+        this->start = nullptr;
+        this->count = 0;
+    }
+
+    int getCount()
+    {
+        return this->count;
+    }
+
+    // to next
+    void push_start(QItem1 _item)
+    {
+        if (this->start == nullptr) {
+            QueueItem<QItem1>* newItem = new QueueItem<QItem1>(_item);
+
+            this->start = newItem;
+            this->end   = newItem;
+
+            newItem->next = nullptr;
+            newItem->prev = nullptr;
+        } else {
+            QueueItem<QItem1>* newItem = new QueueItem<QItem1>(_item);
+
+            this->start->next = newItem;
+            newItem->prev = this->start;
+            newItem->next = nullptr;
+            // do step
+            this->start = newItem;
+        }
+        this->count++;
+    }
+
+    QItem1 pop_start()
+    {
+        if (this->start == nullptr) {
+            throw "Warning. The queue is empty.";
+        }
+
+        QItem1 result = this->start->item;
+        if (this->start == this->end) {
+            delete this->start;
+
+            this->start = nullptr;
+            this->end   = nullptr;
+        } else {
+            QueueItem<QItem1> *ptr = this->start;
+
+            this->start = ptr->prev;
+            this->start->next = nullptr;
+
+            delete ptr;
+        }
+        this->count--;
+
+        return result;
+    }
+
+/*
+    // prev
+    void push_end(QItem1 _item)
+    {
+        if (this->end == nullptr) {
+            this->end = new QueueItem(_item);
+            this->start = this->end;
+            this->end->prev = nullptr;
+            this->end->next = nullptr;
+        } else {
+            this->end->prev = new QueueItem(_item);
+            this->end->prev->prev = nullptr;
+            this->end->prev->next = this;
+            this->end = this->end->prev;
+        }
+    }/**/
+    QItem1 pop_end()
+    {
+        if (this->end == nullptr) {
+            throw "Warning. The queue is empty.";
+        }
+
+        QItem1 result = this->end->item;
+        if (this->end == this->start) {
+            delete this->end;
+            this->end = nullptr;
+            this->start = nullptr;
+        } else if (this->end != this->start) {
+            QueueItem<QItem1>* ptr = this->end;
+
+            this->end = this->end->next;
+            this->end->prev = nullptr;
+
+            delete ptr;
+        }
+        this->count--;
+
+        return result;
+    }
+};
+
+#endif //NBE_CHAT_SERVER_MYQUEUE_H
--- tests/MyQueueTest.cpp
+++ tests/MyQueueTest.cpp
@@ -3,10 +3,32 @@
 
 #include "../include/tmp/MyQueue.h"
 
-TEST(MyQueueTest, CreateEmptyQueue)
+class MyQueueTest: public testing::Test
 {
-    MyQueue<int> mq1;
-    mq1.push_start(77);
+protected:
 
-    EXPECT_EQ(mq1.pop_start(), 77);
+    MyQueueTest()
+    {
+        mq1.push_start(77);
+        mq1.push_start(78);
+        mq1.push_start(79);
+
+        mq2.push_start(11);
+//        mq2.push_end(11)
+    }
+
+    MyQueue<int> mq1;
+    MyQueue<int> mq2;
+    MyQueue<int> mq3;
+};
+
+TEST_F(MyQueueTest, CreateMq3)
+{
+    ASSERT_EQ(mq1.getCount(), 3);
+    mq1.pop_start();
+    mq1.pop_start();
+    mq1.pop_start();
+    ASSERT_EQ(mq1.getCount(), 0);
+
+    ASSERT_THROW(mq1.pop_start(), const char*);
 }
